081ed068c475704f9cfe0aba449319e079b02575
João Vítor <joaovitor502013@gmail.com>
2024-12-11 17:03:20 -0300

    Nova senha e remoção do estado distração

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 // Código correto e de distração
-const correctCode = "77777";
-const distractionCodes = ["12345", "54321", "99999"];
+const correctCode = "7216";
 
 // Pegando elementos da página
 const input = document.getElementById("codeInput");
@@ -10,7 +9,6 @@ const button = document.getElementById("submitButton");
 // Sons
 const successSound = new Audio('sons/som_sucesso.wav');
 const errorSound = new Audio('sons/som_erro.wav');
-const distractionSound = new Audio('sons/som_distração.wav');
 
 // Função para efeitos visuais
 function applyVisualEffect(type) {
@@ -21,9 +19,6 @@ function applyVisualEffect(type) {
   } else if (type === "error") {
     body.style.backgroundColor = "#FF101F";
     setTimeout(() => (body.style.backgroundColor = "#1a1a1a"), 5000);
-  } else if (type === "distraction") {
-    body.style.backgroundColor = "#FFD639";
-    setTimeout(() => (body.style.backgroundColor = "#1a1a1a"), 10000);
   }
 }
 
@@ -36,11 +31,6 @@ function validateCode() {
     applyVisualEffect("success");
     feedback.style.color = "#00AF54";
     feedback.textContent = "🎉 Parabéns! Você conseguiu escapar! 🎯";
-  } else if (distractionCodes.includes(inputCode)) {
-    distractionSound.play();
-    applyVisualEffect("distraction");
-    feedback.style.color = "#FFD639";
-    feedback.textContent = "😂 Hahaha! Esse código está correto, mas é apenas uma distração! 🤡";
   } else {
     errorSound.play();
     applyVisualEffect("error");
